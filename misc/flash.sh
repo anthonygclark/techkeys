c3e95a6010cf06101cb3a0ab5cbccd6966b3aa9f
Anthony Clark <clark.anthony.g@gmail.com>
2014-06-28 00:13:23 -0400

    Better flash script

--- misc/flash.sh
+++ misc/flash.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
+# export for task_source
+export logFile="flash.log"
+> "$logFile"
+
 HEX="$1"
 
 [[ -z $HEX ]] && {
@@ -11,19 +15,35 @@ HEX="$1"
     exit 1
 }
 
-echo "Flashing..."
-sudo -v || exit 1
 
-export logFile="flash.log"
-source $(dirname $(readlink -m ${BASH_SOURCE[0]}))/task_source || { 
-    echo "need task_source"
+which dfu-programmer &>/dev/null || {
+    echo "Error: Need 'dfu-programmer'"
     exit 1
 }
 
-if [[ $(lsusb | grep -c Atmel) -gt 1 ]]; then
-    echo "There are multiple devices present, please removal all but one"
-    echo " and try again"
+function __die()
+{
+    echo "Error: $1" 1>2
     exit 1
+}
+
+function __die_log()
+{
+    echo "Error: See log"
+    echo "---------- LOG ----------"
+    cat "$logFile"
+    exit 1
+}
+
+echo "Flashing..."
+sudo -v || exit 1
+
+source $(dirname $(readlink -m ${BASH_SOURCE[0]}))/task_source || { 
+    __die "need 'task_source'"
+}
+
+if [[ $(lsusb | grep -c Atmel) -gt 1 ]]; then
+    __die "There are multiple devices present, please removal all but one and try again"
 fi
 
 function poll()
@@ -44,7 +64,13 @@ function uploadit()
     sudo dfu-programmer atmega16u2 flash ${HEX} --debug-level 5
 } &>> "$logFile"
 
+function resetit()
+{
+    sleep 1
+    sudo dfu-programmer atmega16u2 reset
+} &>> "$logFile"
 
 poll & waitProgress "Waiting for DFU mode (connect device and jump the pads!)"  || exit 1
-flashit & waitProgress "Erasing..."                                             || exit 1
-uploadit & waitProgress "Uploading..."                                          || exit 1
+flashit & waitProgress "Erasing..."                                             || __die_log
+uploadit & waitProgress "Uploading..."                                          || __die_log
+resetit & waitProgress "Resetting..."                                           || __die_log
