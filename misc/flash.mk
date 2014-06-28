2f2ac4b85865f40b093cd9ec439ab960cb52c870
Anthony Clark <aclark@bbn.com>
2014-06-28 00:13:23 -0400

    Better flash script

--- misc/flash.mk
+++ misc/flash.mk
@@ -1,4 +1,4 @@
 # Make sure BINARY is set to the absolute path of the output hex file
 # Make sure MISC_PATH is the absolute or relative path of the misc dir
-flash:
+flash: HEXBUILD
 	@bash $$(readlink -m $(MISC_PATH))/flash.sh $$(readlink -m $(HEX)) 
